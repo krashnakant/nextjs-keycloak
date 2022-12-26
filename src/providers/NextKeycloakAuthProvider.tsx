4108aac73ed65e616f1481f1e3c8cc4715e04cf9
Krashna Chaurasia <krashna.chaurasia@yahoo.com>
2022-12-26 17:28:35 +0100

    Revert "upgrade dependencies"

--- src/providers/NextKeycloakAuthProvider.tsx
+++ src/providers/NextKeycloakAuthProvider.tsx
@@ -1,23 +1,31 @@
-import Keycloak, { KeycloakConfig, KeycloakInitOptions } from "keycloak-js";
-import React, { FC, useMemo } from "react";
-import { useInitKeycloak } from "../hooks/UseInitialKeycloak";
+import Keycloak, {
+  KeycloakConfig,
+  KeycloakInitOptions,
+} from 'keycloak-js';
+import React, { FC, useMemo } from 'react';
+import { useInitKeycloak } from '../hooks/UseInitialKeycloak';
 
-import { getKeycloakInstance } from "../utils/KeycloakInstance";
-import { NextKeycloakAuthContext } from "./NextKeycloakAuthContext";
+import { getKeycloakInstance } from '../utils/KeycloakInstance';
+import { NextKeycloakAuthContext } from './NextKeycloakAuthContext';
 
-export interface IKeycloakAuthenticationProviderProps {
+interface IKeycloakAuthenticationProviderProps {
   config: KeycloakConfig;
   initOption?: KeycloakInitOptions;
   children: React.ReactNode;
 }
 
-export const NextKeycloakAuthProvider: FC<
-  IKeycloakAuthenticationProviderProps
-> = ({ children, config, initOption }) => {
+export const NextKeycloakAuthProvider: FC<IKeycloakAuthenticationProviderProps> = ({
+  children,
+  config,
+  initOption,
+}) => {
   const keycloak = useMemo(() => {
     return getKeycloakInstance(config);
   }, [config]);
-  const initializedKeycloak = useInitKeycloak(keycloak as Keycloak, initOption);
+  const initializedKeycloak = useInitKeycloak(
+    keycloak as Keycloak,
+    initOption
+  );
   return (
     <NextKeycloakAuthContext.Provider value={initializedKeycloak}>
       {children}
